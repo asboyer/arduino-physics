2636e178dc3cea49afdd585d27b9143d134d8529
boyer <asboyer@gmail.com>
2021-06-10 16:15:47 -0400

    code for ball

--- crystal_ball/crystal_ball.ino
+++ crystal_ball/crystal_ball.ino
@@ -10,27 +10,82 @@ int reply;
 void setup() {
   lcd.begin(16, 2);
   pinMode(switchPin, INPUT);
-  lcd.print("Eric and Boyer's crystal ball!")
-  lcd.print("Ask the")
+  lcd.print("Eric and Boyer's crystal ball!");
+  lcd.print("Ask the");
   lcd.setCursor(0, 1);
   lcd.print("Crystal Ball!");
 }
 
 void loop() {
   switchState = digitalRead(switchPin);
-  if(switchState != prevSwitchState){
-    if(switchState == LOW){
-        reply = random(8);
-        lcd.clear();
-        lcd.setCursor(0, 0);
-        lcd.print("The ball says: ");
-        lcd.setCursor(0, 1):
-        switch(reply){
-          case 0;
-          lcd.print("Yes");
-          break;
-          case 1;
-        }
+  if(switchState != prevSwitchState && switchState == LOW){
+    reply = random(8);
+    lcd.clear();
+    lcd.setCursor(0, 0);
+    lcd.print("The ball says: ");
+    lcd.setCursor(0, 1);
+    switch(reply){
+      case 0:
+      lcd.print("It is certain.");
+      break;
+      case 1:
+      lcd.print("It is decidedly so.");
+      break;
+      case 2:
+      lcd.print("Without a doubt.");
+      break;
+      case 3:
+      lcd.print("Yes - definitely.");
+      break;
+      case 4:
+      lcd.print("You may rely on it.");
+      break;
+      case 5:
+      lcd.print("As I see it, yes.");
+      break;
+      case 6:
+      lcd.print("Most likely.");
+      break;
+      case 7:
+      lcd.print("Outlook good.");
+      break;
+      case 8:
+      lcd.print("Yes.");
+      break;
+      case 9:
+      lcd.print("Signs point to yes.");
+      break;
+      case 10:
+      lcd.print("Reply hazy, try again.");
+      break;
+      case 11:
+      lcd.print("Ask again later.");
+      break;
+      case 12:
+      lcd.print("Better not tell you now.");
+      break;
+      case 13:
+      lcd.print("Cannot predict now.");
+      break;
+      case 14:
+      lcd.print("Concentrate and ask again.");
+      break;
+      case 15:
+      lcd.print("Don't count on it.");
+      break;
+      case 16:
+      lcd.print("My reply is no.");
+      break;
+      case 17:
+      lcd.print("My sources say no.");
+      break;
+      case 18:
+      lcd.print("Outlook not so good.");
+      break;
+      case 19:
+      lcd.print("Very doubtful.");
+      break;
     }
   }
+  prevSwitchState = switchState;
 }
