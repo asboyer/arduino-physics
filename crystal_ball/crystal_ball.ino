fef505077bab97b3b9c9b3fb4d7c977e4873740a
boyer <asboyer@gmail.com>
2021-06-10 15:52:34 -0400

    reorginization + starting crystal ball

---
+++ crystal_ball/crystal_ball.ino
@@ -0,0 +1,14 @@
+#include <LiquidCrystal.h>
+
+const int 
+
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
